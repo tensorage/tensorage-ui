9281a1b065b4ff69a55eda1124cad5ba998d84bd
salahawk <bstuqopavyrwku8251@gmail.com>
2023-11-21 13:09:18 +0000

    feat: clear temp data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const fileToDataUrl = (file) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      resolve(event.target.result);
-    };
-    reader.onerror = (error) => {
-      reject(error);
-    };
-    reader.readAsDataURL(file);
-  });
-};
-
-
 function App() {
   const [hash, setHash] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
